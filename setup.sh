0882d52e197c04700ce94396cac18a5d2cc79317
Michael Holzinger <mike.holzinger@gmail.com>
2016-05-14 14:33:42 -0700

    Added logic to prevent multiple rule additions

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # Our config file for the 8bitdo controller
-CFG=8BitdoNES30Pro8BitdoNES30Pro.cfg
+CFG_USB=8Bitdo-NES30-Pro-GamePad-Bluetooth-Mode.cfg
+CFG_BT=8Bitdo-NES30-Pro-GamePad-USB-Mode.cfg
 
 # Target path for the file
 CONFIG_TARGET=/opt/retropie/configs/all/retroarch-joypads
 
 # We are adding a new rules file force the SDL to see the 8bitdo as a joypad.
 # Naming the rules file 10-local.rules to get it to load earlier then the other rules
+
+
+if [[ ! -f /etc/udev/rules.d/10-local.rules ]]; then 
+then echo "Creating rule /etc/udev/rules.d/10-local.rules"
 
 cat >> /etc/udev/rules.d/10-local.rules <<EOL
 # Add more 8bitdo Joystick ID's to this config file
@@ -17,13 +22,18 @@ SUBSYSTEM=="input", ATTRS{name}=="8Bitdo SNES30 GamePad Joystick", MODE="0666", 
 
 EOL
 
-#Time to copy over a known working 8bitdo config file!
+# Time to copy over a known working 8bitdo config file!
 
 # Copy the original config file if exists 
-if [ -f $CONFIG_TARGET/$CFG ]
-  then mv $CONFIG_TARGET/$CFG $CONFIG_TARGET/$CFG.$(date +%M%H%S).bak
+if [ -f $CONFIG_TARGET/$CFG_USB ]
+  then mv $CONFIG_TARGET/$CFG_USB $CONFIG_TARGET/$CFG_USB.$(date +%M%H%S).bak
+fi
+
+if [ -f $CONFIG_TARGET/$CFG_BT ]
+  then mv $CONFIG_TARGET/$CFG_BT $CONFIG_TARGET/$CFG_BT.$(date +%M%H%S).bak
 fi
 
 # Copy the config file into the target directory for retroarch
-cp configs/$CFG $CONFIG_TARGET/$CFG
+cp configs/$CFG_USB $CONFIG_TARGET/$CFG_USB
+cp configs/$CFG_BT $CONFIG_TARGET/$CFG_BT
 
